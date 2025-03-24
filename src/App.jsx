957a05d42c527f09b4633252698dd28c9f30e2dc
Aynaz Momtazi <Aymzzi2019@gmail.com>
2025-03-25 01:08:05 +0300

    admin page created

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { BrowserRouter} from "react-router-dom";
 import { defaultOptions } from "configs/reactQueryConfigs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Layout from "./layouts/Layout";
 
 function App() {
   const queryClient = new QueryClient({dafaultOptions: defaultOptions})
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Router />
+        <Layout>
+          <Router />
+        </Layout>
       </BrowserRouter>
       <ReactQueryDevtools />
     </QueryClientProvider>
