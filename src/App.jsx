01974b31b2daa9140ad4474fb182050fab0c1a28
Aynaz Momtazi <Aymzzi2019@gmail.com>
2025-03-24 20:36:40 +0300

    getting and setting accessToken compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { BrowserRouter} from "react-router-dom";
 import Router from "router/Router";
+import { BrowserRouter} from "react-router-dom";
+import { defaultOptions } from "configs/reactQueryConfigs";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient({dafaultOptions: defaultOptions})
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
 )}
 
 export default App;
